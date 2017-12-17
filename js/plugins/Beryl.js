1149ba464b49754be500df1287b82e39d19bf69b
Joy Diamond <python.gem@gmail.com>
2017-12-16 20:41:49 -0500

    Change clarity mode to use $ in front of all variables

--- js/plugins/Beryl.js
+++ js/plugins/Beryl.js
@@ -19,50 +19,53 @@ window.Gem = {
 
 
 //
-//  In Gem "clarity" mode, *every* object created has a `.name` and `.summary` member to help introspect the object
+//  In Gem "clarity" mode, *every* object created has a `.$name` and `.$summary` member to help introspect the object
 //  in developer tools:
 //
 //      This makes it a lot clearer what the object is used for.
 //
-//      Also each module appears in `Gem.modules.ModuleName`, with each member of that module having a
-//      `.name` & `.summary` members.
+//      All "clarity" objects begin with `$` (see below for an exeption, when instead `.__name__`
+//      or `.__summary__` is used to avoid conflicts).
+//
+//      Also each module appears in `Gem.$modules.ModuleName`, with each member of that module having a
+//      `.$name` & `.$summary` members.
 //
 //      By *every* object this includes all closure objects that are used.  This make it very easy to examine
 //      the `.[[Scopes]]` member of a function and introspect the value of each of it's closure objects.
 //
-//      When an object uses `.name` or `.summary` members for it's own purposes, then the extra members created
+//      When an object uses `.$name` or `.$summary` members for it's own purposes, then the extra members created
 //      are named `.__name__` and `.__summary__` to avoid conflicts.
 //
 if (Gem.clarity) {
-    Gem.name    = 'Gem'                                     //  Name of this variable.
-    Gem.summary = 'The only global variable used by Gem.'   //  What `Gem` is used for
+    Gem.$name    = 'Gem'                                     //  Name of this variable.
+    Gem.$summary = 'The only global variable used by Gem.'   //  What `Gem` is used for
 
-    Gem.modules = {                                         //  Modules (clarity mode only)
-        name    : 'Gem.modules',
-        summary : 'Map of introspection of all the Gem modules (clarity mode only).',
-        Gem     : {
-            name    : 'Gem.modules.Gem',
-            summary : 'An introspection of the Gem module (clarity mode only).'//,
+    Gem.$modules = {                                         //  Modules
+        $name    : 'Gem.$modules',
+        $summary : 'Map of introspection of all the Gem modules.',
+        Beryl   : {
+            $name    : 'Gem.$modules.Beryl',
+            $summary : 'An introspection of the Beryl module.'//,
         }//,
     }
 
-    Gem.scripts.name    = 'Gem.scripts'
-    Gem.scripts.summary = 'Map of all the scripts loaded (or loading).'
+    Gem.scripts.$name    = 'Gem.scripts'
+    Gem.scripts.$summary = 'Map of all the scripts loaded (or loading).'
 }
 
 
 //
 //  Bootstrap `Gem.codify`
 //
-//      The reason the function is named `Gem__codify` (meaning `Gem.codify`) is so that it shows
-//      up in stack traces as the full name `Gem__codify` instead of shorter name `codify`
+//      The reason the function is named `Beryl__codify` (meaning `Gem.codify`) is so that it shows
+//      up in stack traces as the full name `Beryl__codify` instead of shorter name `codify`
 //      (this is really really helpful when reading stack traces).
 // 
-Gem.codify = function Gem__codify(name, summary, codifier) {
+Gem.codify = function Beryl__codify(name, $summary, codifier) {
     Gem[name] = codifier()
 
     if (Gem.clarity) {
-        Gem.modules.Gem[name] = { name : name, summary : summary, code : Gem[name] }
+        Gem.$modules.Beryl[name] = { $name : name, $summary : $summary, $code : Gem[name] }
     }
 }
 
@@ -73,15 +76,34 @@ Gem.codify = function Gem__codify(name, summary, codifier) {
 //      One time only: Uses previously defined bootstrap version of `Gem.codify`.
 // 
 Gem.codify(
-    'codify',
-    'Create the code for a function or procedure, typically as a closure to avoid the use of any global variables.',
-    function codify__Gem__codify() {
+    'produce_codify',
+    (
+          'Produce a "codify" function for a specific module'
+        + '.  A "codify" function creates the code for a function or procedure'
+        + ', typically as a closure to avoid the use of any global variables'
+        + '.'
+    ),
+    function codifier__Beryl__produce_codify() {
+        var Gem = window.Gem
+
+
+        if (Gem.clarity) {
+            var $name    = '<closure Gem.$modules.ModuleName.produce_codify>'
+            var $summary = (
+                      'The closure for `Gem.$modules.ModuleName.produce_codify`'
+                    + '.  Contains all closure variables *EXCEPT* `$module`'
+                    + ' (`$module` is created in a different closure)'
+                    + '.'
+                )
+
+            function _force_$name_and_$summary_to_appear_in_the_closure() { return $name + $summary }
+        }
+
+
         var create_object = Object.create
-        var Gem           = window.Gem
-        var module        = Gem.modules.Gem
 
 
-        function _create_enumerable_property_with_uninitialized_value(name) {
+        function _create_enumerable_property_with_uninitialized_value($name) {
             //
             //  `r` is a alias for `result`, less typing ...
             //
@@ -97,15 +119,15 @@ Gem.codify(
                 Object.defineProperties(
                         r,
                         {
-                            name : {
-                                    value : 'Gem.codify.properties.' + name,
+                            $name : {
+                                    value : 'Gem.codify.properties.' + $name,
                                     enumerable : true//,
                                 },
 
-                            summary : {
+                            $summary : {
                                     value : (
-                                              'Property descriptor used to initialize the'
-                                            + ' `.' + name + '` attribute of a new member of `Gem.modules.Gem`.'
+                                              'Property descriptor used to initialize the `.' + $name + '`'
+                                             + ' attribute of a new member of `Gem.$modules.ModuleName`.'
                                         ),
 
                                     enumerable : true//,
@@ -118,16 +140,16 @@ Gem.codify(
         }
 
 
-        var property_name    = _create_enumerable_property_with_uninitialized_value('name')
-        var property_summary = _create_enumerable_property_with_uninitialized_value('summary')
-        var property_code    = _create_enumerable_property_with_uninitialized_value('code')
+        var property_$name    = _create_enumerable_property_with_uninitialized_value('$name')
+        var property_$summary = _create_enumerable_property_with_uninitialized_value('$summary')
+        var property_$code    = _create_enumerable_property_with_uninitialized_value('$code')
 
         var properties = create_object(
                 null,
                 {
-                    name    : { value : property_name,    enumerable : true },
-                    summary : { value : property_summary, enumerable : true },
-                    code    : { value : property_code,    enumerable : true }//,
+                    $name    : { value : property_$name,    enumerable : true },
+                    $summary : { value : property_$summary, enumerable : true },
+                    $code    : { value : property_$code,    enumerable : true }//,
                 }//,
             )
             
@@ -138,62 +160,100 @@ Gem.codify(
             //      Two members `.__name__` & `.__summary__` are to document `Gem.codify.properties`, and are thus
             //      *NOT* enumerable.
             //
-            //      Three members `.name`, `.summary`, and `.code` are to be used to create other attributes, and
+            //      Three members `.$name`, `.$summary`, and `.$code` are to be used to create other attributes, and
             //      thus are enumerable.
             //      
             //      Thus when `Gem.codify.properties` is used to add attributes to an object, it only adds the
-            //      three enumerable attributes (i.e.: `.name`, `.summary`, and `.code`).
+            //      three enumerable attributes (i.e.: `.$name`, `.$summary`, and `.$code`).
             //
             Object.defineProperties(
                     properties,
                     {
                         __name__ : {
-                                value      : 'Gem.codify.properties',
+                                value        : 'Gem.codify.properties'//,
                                 //enumerable : false//,     //  Only to document `properties`, hence not enumerable
                             },
 
                         __summary__ : {
-                                value : 'Property descriptors used to initialize a new member of `Gem.modules.Gem`.',
+                                value : (
+                                          'Property descriptors used to initialize'
+                                         + ' a new member of `Gem.$modules.ModuleName`.'
+                                    )//,
                                 //enumerable : false//,     //  Only to document `properties`, hence not enumerable
                             }//,
                     }//,
                 )
         }
 
+        return function Beryl__produce_codify($module) {
+            if (Gem.clarity) {
+                var $name    = '<closure Gem.$modules.ModuleName.codify>'
+                var $summary = (
+                          'The closure for `Gem.$modules.ModuleName.codify`'
+                        + '.  Contains `$module` which is the module this codify function is specifically for'
+                        + '.'
+                    )
 
-        return function Gem__codify(name, summary, codifier) {
-            //
-            //  Create the code for a function or procedure, typically as a closure to avoid the use of any global
-            //  variables.
-            //
-            var code = Gem[name] = codifier()
+                function _force_$name_and_$summary_to_appear_in_the_closure() { return $name + $summary }
+            }
 
-            property_name   .value = name
-            property_summary.value = summary
-            property_code   .value = code
+            return function Beryl__codify(name, $summary, codifier) {
+                //
+                //  Create the code for a function or procedure, typically as a closure to avoid the use of any global
+                //  variables.
+                //
+                var $code = Gem[name] = codifier()
 
-            module[name] = create_object(null, properties)
+                property_$name   .value = name
+                property_$summary.value = $summary
+                property_$code   .value = $code
 
-            //
-            //  Delete these unused attributes, for two reasons:
-            //
-            //      1.  So they do not appear when introspecting in Developer Tools; AND
-            //      2.  So the can be properly garbage collected if `Gem.modules.Gem[name]` is deleted.
-            //
-            delete property_name   .value
-            delete property_summary.value
-            delete property_code   .value
+                $module[name] = create_object(null, properties)
+
+                //
+                //  Delete these unused attributes, for two reasons:
+                //
+                //      1.  So they do not appear when introspecting in Developer Tools; AND
+                //      2.  So the can be properly garbage collected if `Gem.$modules.Beryl[name]` is deleted.
+                //
+                delete property_$name   .value
+                delete property_$summary.value
+                delete property_$code   .value
+            }
         }
     }
 )
 
-//
-//  Now run `Gem.codify` on `Gem.codify` (so it uses it's newly defined itself on itself).
-//
+
 Gem.codify(
-    Gem.modules.Gem.codify.name,
-    Gem.modules.Gem.codify.summary,
-    function codify__Gem__codify() {
-        return Gem.modules.Gem.codify.code
+    'codify',
+    (
+          'Create the code for a function or procedure'
+        + ', typically as a closure to avoid the use of any global variables.'
+    ),
+    function codifier__Beryl__codify() {
+        return Gem.produce_codify(Gem.$modules.Beryl)
     }
 )
+
+
+//
+//  Now run `Gem.codify` on `Gem.produce_codify` & `Gem.codify` (so it uses it's newly defined itself on itself).
+//
+Gem.codify(
+    Gem.$modules.Beryl.produce_codify.$name,
+    Gem.$modules.Beryl.produce_codify.$summary,
+    function codifier__Beryl__produce_codify() {
+        return Gem.$modules.Beryl.produce_codify.$code
+    }
+)
+
+Gem.codify(
+    Gem.$modules.Beryl.codify.$name,
+    Gem.$modules.Beryl.codify.$summary,
+    function codifier__Beryl__codify() {
+        return Gem.$modules.Beryl.codify.$code
+    }
+)
+
+console.log(Gem.$modules.Beryl)
